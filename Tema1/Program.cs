625da335ae885e40f6937b6ae0fcf9a2e213b651
MARIAN <MARIAN@DESKTOP-LJO9UPV>
2019-12-12 15:43:35 +0200

    Add project files.

---
+++ Tema1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int month;
+            string nameMonth;
+            //  Console.WriteLine("Cititi luna de nastere(intre 1 si 12):");
+
+            do
+            {
+                Console.WriteLine("Cititi luna de nastere(intre 1 si 12):");
+                nameMonth = Console.ReadLine();
+                month = Convert.ToInt32(nameMonth);
+
+            } while (month > 12 || month < 1);
+
+
+            int day;
+            string nameDay;
+            do
+            {
+                Console.WriteLine("Cititi ziua de nastere(intre 1 si 31,depinzand de luna):");
+                nameDay = Console.ReadLine();
+                day = Convert.ToInt32(nameDay);
+            } while (day < 1 || day > 31);
+
+
+
+            int year;
+            string nameYear;
+            do
+            {
+                Console.WriteLine("Cititi anul nasterii(intre 0 si 2019):");
+                nameYear = Console.ReadLine();
+                year = Convert.ToInt32(nameYear);
+            } while (year < 1 || year > 2019);
+
+            string letter;
+            do
+            {
+                Console.WriteLine("Cititi genul");
+                letter = Console.ReadLine();
+            } while (letter != "M" && letter != "F");
+            int ageOfPerson;
+            genders? gender = null;
+
+            DateTime now = DateTime.Now;
+
+            DateTime birthday = new DateTime(year, month, day);
+
+            Console.WriteLine(birthday.ToString("dd/MMM/yyyy"));
+
+
+            if (now.Month <= birthday.Month && now.Day <= birthday.Day)
+            {
+                ageOfPerson = now.Year - birthday.Year;
+            }
+            else
+            {
+                ageOfPerson = now.Year - birthday.Year - 1;
+            }
+            Console.WriteLine("Persoana are: " + ageOfPerson + " ani.");
+
+            if (letter == "M")
+            {
+                gender = genders.Male;
+                if (ageOfPerson > 65)
+                {
+                    Console.WriteLine("Barbat pensioant");
+                }
+                else
+                {
+                    Console.WriteLine("Barbatul se va pensiona la 65 ani");
+                }
+            }
+            else if (letter == "F")
+            {
+                gender = genders.Female;
+                if (ageOfPerson > 63)
+                {
+                    Console.WriteLine("Femeie pensioanta");
+                }
+                else
+                {
+                    Console.WriteLine("Femeia se va pensiona la 63 ani");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Gen inexistent");
+            }
+
+
+
+
+        }
+
+        public enum genders
+        {
+            Male,
+            Female
+        }
+
+    }
+}
---
+++ Tema2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Tema";
+            string s2 = "numarul";
+            string s3 = "doi";
+            string s4 = "tema";
+            string s5 = "Tema numarul 2";
+            //Console.WriteLine(s1 + " " + s2+ " " + s3);
+
+            string emptyString = string.Empty;
+
+            /*
+            if (string.IsNullOrEmpty(emptyString))
+            {
+                Console.WriteLine("Stringul este gol sau null");
+            }
+            else
+            {
+                Console.WriteLine("Stringul nu este gol sau null ");
+            }
+            if (string.IsNullOrEmpty(s1))
+            {
+                Console.WriteLine("Stringul este gol sau null");
+            }
+            else
+            {
+                Console.WriteLine("Stringul nu este gol sau null ");
+            }
+            */
+
+            /*
+            if (string.Compare(s1,s4) == 0)
+            {
+                Console.WriteLine("Stringuri identice");
+            }
+            else
+            {
+                Console.WriteLine("Stringuri diferite");
+            }
+            */
+
+            /*
+            string replacedString = s5.Replace("Tema", "Sarcina");
+            Console.WriteLine(s5);
+            Console.WriteLine(replacedString);
+            */
+
+            /*
+            string concatenatedString = string.Concat(s1," ", s2," ", s3);
+            Console.WriteLine(concatenatedString);
+            */
+
+            /*
+            string upperCaseString = s5.ToUpper();
+            string lowerCaseString = s5.ToLower();
+            Console.WriteLine("Sirul scris cu litere mari: " + upperCaseString);
+            Console.WriteLine("Sirul scris cu litere mici: " + lowerCaseString);
+            */
+
+            /*
+            string searchString = "numarul";
+            if(s5.Contains(searchString))
+            {
+                Console.WriteLine("Textul contine string ul cautat");
+            }
+            else
+            {
+                Console.WriteLine("Textul nu contine string ul cautat");
+            }
+            */
+
+        }
+    }
+}
